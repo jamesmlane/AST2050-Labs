6abda1b0762543bc1598ba38433233edf98416f7
James Lane <jameslane93@gmail.com>
2019-01-17 13:14:39 -0500

    Initialize project-specific module & reading function

---
+++ src/ast2050/lab1.py
@@ -0,0 +1,49 @@
+# ----------------------------------------------------------------------------
+#
+# TITLE - lab1.py
+# PROJECT - AST 2050
+# CONTENTS:
+#
+# ----------------------------------------------------------------------------
+
+### Docstrings and metadata:
+'''Functions for Lab 1 of AST 2050
+
+Import me by including this at the beginning of the script:
+sys.path.append('path/to/src/')
+import ast2050.lab1
+'''
+
+### Imports
+
+## Basic
+import numpy as np
+import sys, os, pdb
+# import copy
+# import glob
+# import subprocess
+
+# Imaging
+from PIL import Image
+
+## Plotting
+from matplotlib import pyplot as plt
+
+
+# ----------------------------------------------------------------------------
+
+def read_tiff(filename):
+    '''read_tiff:
+    
+    Read a .tiff file and return a numpy array
+    
+    Args:
+        filename (string) - File name (with .tiff ending)
+    
+    Returns:
+        image (float array) - Image as a numpy array
+    '''
+    return np.array( Image.open( filename ) )
+#def
+
+# ----------------------------------------------------------------------------
