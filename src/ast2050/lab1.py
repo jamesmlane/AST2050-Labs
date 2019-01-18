be7e7448a92d244de8b7bba20097e4aad615a923
James Lane <jameslane93@gmail.com>
2019-01-18 00:05:52 -0500

    Add functions for background subtraction and SPE detection

--- src/ast2050/lab1.py
+++ src/ast2050/lab1.py
@@ -47,3 +47,36 @@ def read_tiff(filename):
 #def
 
 # ----------------------------------------------------------------------------
+
+def single_pixel_center_difference(image):
+    '''single_pixel_center_difference:
+    
+    For each pixel subtract the 8 surrounding pixels from the central pixel.
+    Return a map of the same size but with this metric calculated.
+    
+    Args:
+        image (float array) - Input image
+        
+    Returns:
+        metric (float array) - Single pixel detection metric array
+    '''
+    
+    # Shape arrays and output array.
+    lenx, leny = image.shape
+    metric = np.zeros_like(image)
+    
+    # Double loop over each pixel.
+    for i in range( lenx ):
+        for j in range( leny ):
+            
+            # Continue if an edge pixel
+            if i==0 or i==(lenx-1) or j==0 or j==(leny-1): continue
+            
+            # Subtract the pixels around the point 
+            metric[i,j] = 2*image[i,j] - np.sum(image[(i-1):(i+2),(j-1):(j+2)])
+        ###j
+    ###i
+    
+    return metric
+    
+#def
